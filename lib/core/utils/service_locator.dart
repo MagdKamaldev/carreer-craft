2cc64cb7b0aa81f799448efd77982f1bcf9458e5
MagdKamaldev <magddevf@gmail.com>
2024-02-11 21:20:26 +0200

    setup service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:career_craft/core/utils/api_services.dart';
+import 'package:career_craft/features/login/data/repositories/login_repository_implementation.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupLocator() {
+  getIt.registerLazySingleton<ApiServices>(
+    () => ApiServices(
+      Dio(),
+    ),
+  );
+  getIt.registerLazySingleton<LoginRepositoryImplementation>(
+    () => LoginRepositoryImplementation(
+      apiServices: getIt<ApiServices>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:career_craft/core/utils/app_router.dart';
+import 'package:career_craft/core/utils/service_locator.dart';
 import 'package:career_craft/core/utils/themes.dart';
+import 'package:career_craft/features/login/data/repositories/login_repository_implementation.dart';
+import 'package:career_craft/features/login/presentaion/manager/login_cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setupLocator();
   runApp(const CareerCraft());
 }
 
@@ -11,11 +16,20 @@ class CareerCraft extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Career Craft',
-      theme: theme,
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LoginCubit(
+            getIt<LoginRepositoryImplementation>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Career Craft',
+        theme: theme,
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
